8efb6358d84c18bc60efddbbd7601f3892f38e11
Norman Hooper <nhooper@dimagi.com>
2016-08-17 11:01:41 +0200

    Use SimpleCache to persist cases between requests

--- src/rb_proto/server.py
+++ src/rb_proto/server.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 # coding: utf-8
 from __future__ import unicode_literals
+import json
 from flask import Flask
-from src.rb_proto.const import CASES
+from werkzeug.contrib.cache import SimpleCache
+from rb_proto.const import CASES
 
 
 app = Flask(__name__)
+cache = SimpleCache()
+
+
+def get_cases():
+    cases = cache.get('cases')
+    if cases is None:
+        cases = CASES
+        cache.set('cases', cases)
+    return cases
 
 
 @app.route('/')
@@ -21,7 +32,7 @@ def preview():
     """
     Report Builder Prototype async live preview
     """
-    return CASES
+    return json.dumps(get_cases())
 
 
 if __name__ == '__main__':
