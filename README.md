3f0746a63006ea964a0bb7bd84ad93bb02b5ade0
Norman Hooper <nhooper@dimagi.com>
2016-08-17 17:54:42 +0200

    Drop server

--- README.md
+++ README.md
@@ -1,43 +1,14 @@
 Report Builder Prototype
 ========================
 
-Installation
-------------
+Usage
+-----
 
-Create a virtualenv that uses Python 3, because we can:
-
-    $ mkvirtualenv -p python3 rb_proto
-
-
-Start The Server
-----------------
-
-    $ workon rb_proto
-    (rb_proto) $ export PYTHONPATH=src 
-    (rb_proto) $ src/rb_proto/server.py
-
-or, run the Flask debug server:
-
-    (rb_proto) $ export PYTHONPATH=src 
-    (rb_proto) $ export FLASK_APP=rb_proto.server 
-    (rb_proto) $ export FLASK_DEBUG=1 
-    (rb_proto) $ flask run
+Open html/index.html in a modern browser
 
 
 Design Spec
 -----------
 
 [Report Builder Prototype Design](https://docs.google.com/document/d/18cm2wmajcysXNCFopXf0hKvoadC6hPB_gn6JHWixv0A/edit#)
-
-
-Endpoints
----------
-
-* Landing page: <http://127.0.0.1:5000/>
-* Async live preview: <http://127.0.0.1:5000/preview/>
-* API: 
-  * <http://127.0.0.1:5000/api/1/columns/> (GET, POST)
-  * <http://127.0.0.1:5000/api/1columns/NAME/> (GET, DELETE)
-  * <http://127.0.0.1:5000/api/1columns/all_/> (GET, DELETE)
-
 
--- html/js/rb_proto.js
+++ html/js/rb_proto.js
@@ -2,31 +2,35 @@
 var rbProto = function () {
     var self = this;
 
+    self.getColumnTitles = function (columns) {
+        return _.map(columns, function (column) {
+            return {"title": column["label"]};
+        });
+    };
+
     /**
      * ReportConfig is a view model for managing report configuration
      */
     self.ReportConfig = function (options) {
         var self = this;
-        self.allColumns = options["columns"];
+        self.columns = options["columns"];
+        self.rows = options["rows"];
 
         self.refreshPreview = function (columns) {
-            var columnNames = _.map(columns, function (column) {
-                return {"data": column["name"]};
-            });
             $('#preview').DataTable({
                 "destroy": true,  // Recreate the table with the new columns
-                "ajax": "/preview/",
-                "columns": columnNames
+                "data": self.rows,
+                "columns": rbProto.getColumnTitles(columns),
             });
         }
 
         self.shouldShowGraphSelect = ko.observable(false);
         self.shouldShowGroupBy = ko.observable(false);
 
-        self.selectedColumns = ko.observableArray(options["columns"]);
+        self.selectedColumns = ko.observableArray(self.columns);
         self.selectedColumns.subscribe(function (newValue) {
             self.shouldShowGraphSelect(true);
-            self.refreshPreview();
+            self.refreshPreview(newValue);
         });
 
         return self;
