24fb96d1444867483265e676ae820fe87d081d2d
Norman Hooper <nhooper@dimagi.com>
2016-08-19 09:27:53 +0200

    Switch from rows to data to select columns

--- html/js/rb_proto.js
+++ html/js/rb_proto.js
@@ -8,30 +8,39 @@ var rbProto = function () {
         });
     };
 
+    self.getRows = function (data, columns) {
+        var columnNames = _.map(columns, function (column) {
+            return column["name"];
+        });
+        return _.map(data, function (dict) {
+            return _.values(_.pick(dict, columnNames));
+        });
+    }
+
     /**
      * ReportConfig is a view model for managing report configuration
      */
     self.ReportConfig = function (options) {
         var self = this;
         self.columns = options["columns"];
-        self.rows = options["rows"];
+        self.data = options["data"];
 
         self.refreshPreview = function (columns) {
+            $('#preview').empty();
             $('#preview').DataTable({
                 "destroy": true,  // Recreate the table with the new columns
-                "data": self.rows,
+                "data": rbProto.getRows(self.data, columns),
                 "columns": rbProto.getColumnTitles(columns),
             });
         }
 
         self.selectedColumns = ko.observableArray(self.columns);
         self.selectedColumns.subscribe(function (newValue) {
-//            self.refreshPreview(newValue);
+            self.refreshPreview(newValue);
         });
 
         self.selectedGraph = ko.observable('none');
         self.selectedGraph.subscribe(function (newValue) {
-//            self.refreshPreview(newValue);
             self.shouldShowGroupBy(true);
         });
 
